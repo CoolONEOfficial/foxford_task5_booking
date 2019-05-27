ce7870d182f5a88f0a39d35ad1e9e9bc54f20b0a
Fedromig <48389667+Fedromig@users.noreply.github.com>
2019-05-27 20:28:09 +0300

    Update HousingSlider.vue

--- src/components/body/HousingSlider.vue
+++ src/components/body/HousingSlider.vue
@@ -10,4 +10,206 @@
 
 <style scoped lang="scss">
     @import "../../styles/colors";
+    .frm{
+  min-width: 800px;
+}
+.txtn{
+  position: relative;
+  font-family: Segoe UI;
+  font-size: 18px;
+  text-align: left;
+  left: 19%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 70%;
+}
+.scroll{
+ position: relative;
+ height: 10px;
+ line-height: 150px;
+ padding: 0 45px;
+}
+.scroll input {
+ display: none;
+ position: absolute;
+}
+.scroll div {
+ margin-left: auto;
+ margin-right: auto;
+ width: 70%;
+ overflow: hidden;
+ white-space: nowrap;
+ word-wrap: normal;
+ font-size: 0;
+}
+.scroll img{
+ position: relative;
+ right: 0em;
+ width: 18%;
+ height: 100%;
+ transition: .6s;
+ padding-left: 2%;
+}
+.scroll label{
+  text-shadow: black 1px 1px 0,  black -1px -1px 0, 
+               black -1px 1px 0, black 1px -1px 0;
+  color: #FFFFFF;
+  opacity: 0.6;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: calc(3.5vw + 10px);
+  z-index: 1000;
+  position: absolute;
+  padding-left: 62%;
+  margin-top: -15.5%;
+  padding-top: calc(20% - 60px);
+  width: calc(13% - 22px);
+  margin-left: auto;
+  margin-right: auto;
+  right: calc(12.7% - 5px);
+}
+.scroll label:hover{
+  opacity: 1;
+}
+.scroll input:nth-of-type(1):checked ~ label:nth-of-type(2):after,
+.scroll input:nth-of-type(2):checked ~ label:nth-of-type(3):after,
+.scroll input:nth-of-type(3):checked ~ label:nth-of-type(4):after,
+.scroll input:nth-of-type(4):checked ~ label:nth-of-type(5):after,
+.scroll input:nth-of-type(5):checked ~ label:nth-of-type(6):after,
+.scroll input:nth-of-type(6):checked ~ label:nth-of-type(7):after,
+.scroll input:nth-of-type(7):checked ~ label:nth-of-type(8):after,
+.scroll input:nth-of-type(8):checked ~ label:nth-of-type(9):after,
+.scroll input:nth-of-type(9):checked ~ label:nth-of-type(10):after,
+.scroll input:nth-of-type(10):checked ~ label:nth-of-type(11):after,
+.scroll input:nth-of-type(11):checked ~ label:nth-of-type(12):after,
+.scroll input:nth-of-type(12):checked ~ label:nth-of-type(13):after,
+.scroll input:nth-of-type(13):checked ~ label:nth-of-type(14):after,
+.scroll input:nth-of-type(14):checked ~ label:nth-of-type(15):after,
+.scroll input:nth-of-type(15):checked ~ label:nth-of-type(16):after,
+.scroll input:nth-of-type(16):checked ~ label:nth-of-type(17):after,
+.scroll input:nth-of-type(17):checked ~ label:nth-of-type(18):after,
+.scroll input:nth-of-type(18):checked ~ label:nth-of-type(19):after{
+ content: "\3009";
+ position: absolute;
+ right: 0;
+}
+.scroll input:nth-of-type(2):checked ~ label:nth-of-type(1):after,
+.scroll input:nth-of-type(3):checked ~ label:nth-of-type(2):after,
+.scroll input:nth-of-type(4):checked ~ label:nth-of-type(3):after,
+.scroll input:nth-of-type(5):checked ~ label:nth-of-type(4):after,
+.scroll input:nth-of-type(6):checked ~ label:nth-of-type(5):after,
+.scroll input:nth-of-type(7):checked ~ label:nth-of-type(6):after,
+.scroll input:nth-of-type(8):checked ~ label:nth-of-type(7):after,
+.scroll input:nth-of-type(9):checked ~ label:nth-of-type(8):after,
+.scroll input:nth-of-type(10):checked ~ label:nth-of-type(9):after,
+.scroll input:nth-of-type(11):checked ~ label:nth-of-type(10):after,
+.scroll input:nth-of-type(12):checked ~ label:nth-of-type(11):after,
+.scroll input:nth-of-type(13):checked ~ label:nth-of-type(12):after,
+.scroll input:nth-of-type(14):checked ~ label:nth-of-type(13):after,
+.scroll input:nth-of-type(15):checked ~ label:nth-of-type(14):after,
+.scroll input:nth-of-type(16):checked ~ label:nth-of-type(15):after,
+.scroll input:nth-of-type(17):checked ~ label:nth-of-type(16):after,
+.scroll input:nth-of-type(18):checked ~ label:nth-of-type(17):after,
+.scroll input:nth-of-type(19):checked ~ label:nth-of-type(18):after{
+ content: "\3008";
+ position: absolute;
+ left: 0;
+}
+.scroll input:nth-of-type(2):checked ~ div img {right: 20%;}
+.scroll input:nth-of-type(3):checked ~ div img {right: 40%;}
+.scroll input:nth-of-type(4):checked ~ div img {right: 60%;}
+.scroll input:nth-of-type(5):checked ~ div img {right: 80%;}
+.scroll input:nth-of-type(6):checked ~ div img {right: 100%;}
+.scroll input:nth-of-type(7):checked ~ div img {right: 120%;}
+.scroll input:nth-of-type(8):checked ~ div img {right: 140%;}
+.scroll input:nth-of-type(9):checked ~ div img {right: 160%;}
+.scroll input:nth-of-type(10):checked ~ div img {right: 180%;}
+.scroll input:nth-of-type(11):checked ~ div img {right: 200%;}
+.scroll input:nth-of-type(12):checked ~ div img {right: 220%;}
+.scroll input:nth-of-type(13):checked ~ div img {right: 240%;}
+.scroll input:nth-of-type(14):checked ~ div img {right: 260%;}
+.scroll input:nth-of-type(15):checked ~ div img {right: 280%;}
+.scroll input:nth-of-type(16):checked ~ div img {right: 300%;}
+.scroll input:nth-of-type(17):checked ~ div img {right: 320%;}
+.scroll input:nth-of-type(18):checked ~ div img {right: 340%;}
+.scroll input:nth-of-type(19):checked ~ div img {right: 360%;}
+.scroll input:nth-of-type(20):checked ~ div img {right: 380%;}
+
 </style>
+
+<div class="frm">
+  <div class="txtn"> 5,7 миллионов домов, апартаментов и других вариантов жилья... Вы же не думали, что у нас есть только отели? </div>
+
+  <div class="scroll">
+    <input type="radio" id="l0" name="rrr" checked="checked"/>
+    <input type="radio" id="l1" name="rrr"/>
+    <input type="radio" id="l2" name="rrr"/>
+    <input type="radio" id="l3" name="rrr"/>
+    <input type="radio" id="l4" name="rrr"/>
+    <input type="radio" id="l5" name="rrr"/>
+    <input type="radio" id="l6" name="rrr"/>
+    <input type="radio" id="l7" name="rrr"/>
+    <input type="radio" id="l8" name="rrr"/>
+    <input type="radio" id="l9" name="rrr"/>
+    <input type="radio" id="20" name="rrr"/>
+    <input type="radio" id="21" name="rrr"/>
+    <input type="radio" id="22" name="rrr"/>
+    <input type="radio" id="23" name="rrr"/>
+    <input type="radio" id="24" name="rrr"/>
+    <input type="radio" id="25" name="rrr"/>
+    <input type="radio" id="26" name="rrr"/>
+    <input type="radio" id="27" name="rrr"/>
+    <input type="radio" id="28" name="rrr"/>
+    <input type="radio" id="29" name="rrr"/>
+    <input type="radio" id="30" name="rrr"/>
+
+    <label for="l0"></label>
+    <label for="l1"></label>
+    <label for="l2"></label>
+    <label for="l3"></label>
+    <label for="l4"></label>
+    <label for="l5"></label>
+    <label for="l6"></label>
+    <label for="l7"></label>
+    <label for="l8"></label>
+    <label for="l9"></label>
+    <label for="20"></label>
+    <label for="21"></label>
+    <label for="22"></label>
+    <label for="23"></label>
+    <label for="24"></label>
+    <label for="25"></label>
+    <label for="26"></label>
+    <label for="27"></label>
+    <label for="28"></label>
+    <label for="29"></label>
+    <label for="30"></label>
+
+    <div>
+  
+<a href="https://www.booking.com/apartments/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/xDgq5LR/2.png" alt=""/></a>
+  <a href="https://www.booking.com/resorts/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/wSK4JD6/24.png" alt=""/></a>
+<a href="https://www.booking.com/villas/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/F6rgGNM/25.png" alt=""/></a>
+<a href="https://www.booking.com/chalet/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/sRSt7nK/5.png" alt=""/></a>
+<a href="https://www.booking.com/cottages/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/1fL5RHS/7.png" alt=""/></a>
+<a href="https://www.booking.com/glamping/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/X39kJyM/8.png" alt=""/></a>
+<a href="https://www.booking.com/aparthotels/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/KFZTHpT/9.png" alt=""/></a>
+<a href="https://www.booking.com/holiday-homes/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/rxvk545/26.png" alt=""/></a>
+<a href="https://www.booking.com/guest-house/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/gJBFV3X/11.png" alt=""/></a>
+<a href="https://www.booking.com/hostels/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/Js9s1t6/28.png" alt=""/></a>
+  <a href="https://www.booking.com/motels/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/PrMB0Nz/13.png" alt=""/></a>
+  <a href="https://www.booking.com/bed-and-breakfast/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/0nRfPVP/15.png" alt=""/></a>
+  <a href="https://www.booking.com/ryokans/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/p0H9c49/16.png" alt=""/></a>
+  <a href="https://www.booking.com/riad/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/pQXnRDf/17.png" alt=""/></a>
+  <a href="https://www.booking.com/holiday-parks/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/gwRLvrS/18.png" alt=""/></a>
+  <a href="https://www.booking.com/homestay/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/89MTD0w/19.png" alt=""/></a>
+  <a href="https://www.booking.com/campings/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/19GCD3j/20.png" alt=""/></a>
+  <a href="https://www.booking.com/country-houses/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/zS76c64/21.png" alt=""/></a>
+  <a href="https://www.booking.com/farm-holidays/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/HNVwJnC/22.png" alt=""/></a>
+  <a href="https://www.booking.com/boats/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/2Z3Shp4/23.png" alt=""/></a>
+  <a href="https://www.booking.com/camp/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/kShh8Mt/24.png" alt=""/></a>
+  <a href="https://www.booking.com/self-catering/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/WkqGgX3/25.png" alt=""/></a>
+  <a href="https://www.booking.com/tiny-house/index.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEP2AED6AEBiAIBqAIDuAK305nnBcACAQ;sid=229a698ced18da069d5859f2ee832970;from_booking_home_promotion=1&"><img src="https://i.ibb.co/JsFVNRb/26.png" alt=""/></a>
+    </div>
+  </div>
+</div>
