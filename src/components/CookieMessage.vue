9c13a0989ca23150b31f4764a3f88c8071eaeb6b
Fedromig <48389667+Fedromig@users.noreply.github.com>
2019-06-01 06:27:56 +0300

    Update CookieMessage.vue

--- src/components/CookieMessage.vue
+++ src/components/CookieMessage.vue
@@ -1,5 +1,13 @@
 <template>
     <!--TODO: cookie message-->
+    
+    <div class="fix" id="elm2">
+      <div class="container-cookie">
+        <div class="cookie-text"><span>В целях обеспечения наилучшего пользовательского опыта для вас мы собираем и используем </span><a href="https://www.booking.com/general.ru.html?aid=375440;label=yandex-fAeMqEnXAC%2AdldAeWbOMuQ-4506493104;sid=bba30412f9f5614ae3b5065bacb0a08b;tmpl=docs/privacy-policy;from_cb=1" style="color:white">cookie-файлы</a><span> и некоторые другие данные на нашем сайте в технических, аналитических и маркетинговых целях. Продолжая просмотр нашего сайта, вы соглашаетесь на сбор и использование cookie-файлов и других данных нами в соответствии с </span><a href="https://www.booking.com/general.ru.html?aid=375440;label=yandex-fAeMqEnXAC%2AdldAeWbOMuQ-4506493104;sid=bba30412f9f5614ae3b5065bacb0a08b;tmpl=docs/privacy-policy;from_cb=1" style="color:white">Положением о конфиденциальности</a>.</div>
+        <button class="ck" id="btnclose">Закрыть</button>
+      </div>
+    </div>
+    
 </template>
 
 <script>
@@ -13,20 +21,17 @@
 
 <style scoped lang="scss">
     @import "../styles/colors";
-    .fix{
+  .fix{
   position: fixed;
   top: calc(100% - 110px);
   opacity: .75;
-  width: 100.89%;
-  height: calc(100%);
+  height: 100%;
   background-color: #003580;
   margin: -8px;
   min-width: 700px;
 }
 .container-cookie{
   position: relative;
-  width: 100.89%;
-  height: calc(30% + cookie-text.height);
   background-color: #003580;
   text-align: center;
   min-width: 700px;
@@ -60,9 +65,4 @@
 
 
 </style>
-<div class="fix" id="elm2">
-  <div class="container-cookie">
-    <div class="cookie-text"><span>В целях обеспечения наилучшего пользовательского опыта для вас мы собираем и используем </span><a href="https://www.booking.com/general.ru.html?aid=375440;label=yandex-fAeMqEnXAC%2AdldAeWbOMuQ-4506493104;sid=bba30412f9f5614ae3b5065bacb0a08b;tmpl=docs/privacy-policy;from_cb=1" style="color:white">cookie-файлы</a><span> и некоторые другие данные на нашем сайте в технических, аналитических и маркетинговых целях. Продолжая просмотр нашего сайта, вы соглашаетесь на сбор и использование cookie-файлов и других данных нами в соответствии с </span><a href="https://www.booking.com/general.ru.html?aid=375440;label=yandex-fAeMqEnXAC%2AdldAeWbOMuQ-4506493104;sid=bba30412f9f5614ae3b5065bacb0a08b;tmpl=docs/privacy-policy;from_cb=1" style="color:white">Положением о конфиденциальности</a>.</div>
-    <button class="ck" id="btnclose">Закрыть</button>
-  </div>
-    </div>
+
